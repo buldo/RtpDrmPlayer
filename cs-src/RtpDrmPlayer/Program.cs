bbe767ef28390e6e1bd1ba7c880b21b5a2494e20
Roman Buldygin <froooks@gmail.com>
2025-08-20 02:48:43 +0300

    init project

---
+++ cs-src/RtpDrmPlayer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RtpDrmPlayer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
